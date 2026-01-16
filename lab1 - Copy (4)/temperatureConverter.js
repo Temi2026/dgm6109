6a453dd2c80e11623db538f550b39bb0be564dba
Temi2026 <obatoke.t@northeastern.edu>
2026-01-15 18:03:49 -0800

    First commit

---
+++ lab1 - Copy (4)/temperatureConverter.js
@@ -0,0 +1,13 @@
+// Average temperature for Ibadan, Oyo State, Nigeria in March (recent year)
+let fahrenheit = 84;
+
+// Convert Fahrenheit to Celsius
+let celsius = (fahrenheit - 32) * 5 / 9;
+
+// Convert Celsius to Kelvin
+let kelvin = celsius + 273.15;
+
+// Output results to the console
+console.log("Temperature (Fahrenheit): " + fahrenheit);
+console.log("Temperature (Celsius): " + celsius);
+console.log("Temperature (Kelvin): " + kelvin);
