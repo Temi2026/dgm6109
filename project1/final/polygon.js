3cad606006e764eeb0c0574202cf3cea8a999e75
Temi2026 <obatoke.t@northeastern.edu>
2026-02-11 20:58:02 -0800

    My snippet_03 assignment

--- project1/final/polygon.js
+++
@@ -1,9 +0,0 @@
-// polygon.js
-// Helper function for drawing polygons in D3
-
-function drawPolygon(svg, points, color) {
-  svg
-    .append("polygon")
-    .attr("points", points)
-    .attr("fill", color);
-}
